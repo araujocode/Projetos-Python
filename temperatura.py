3f06279181c4267f09acc36caf0cf1a59d8a6770
Bruno Machado <bruno.cadoma@gmail.com>
2023-10-24 11:12:05 -0300

    Alterada a fórmula de Kelvin

--- temperatura.py
+++ temperatura.py
@@ -1,7 +1,7 @@
 celsius = float(input("Digite a temperatura em celsius: "))
 
 fahrenheit = float((celsius * 9/5) + 32)
-kelvin = celsius + 273
+kelvin = celsius + 273.15
 
 print("O valor da temperatura convertida para fahrenheit é:", fahrenheit, "ºF")
 print("O valor da temperatura convertida para Kelvin é:", kelvin, "K")
