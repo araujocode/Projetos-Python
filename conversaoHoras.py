4c2f8709bb92557c46be0a856c99a0159db04281
Bruno Machado <bruno.cadoma@gmail.com>
2023-10-24 11:38:38 -0300

    Conversao de horas para dias, minutos e segundos

---
+++ conversaoHoras.py
@@ -0,0 +1,15 @@
+numero = float(input("Digite um número de horas: "))
+
+horas = numero
+print("Horas:", horas)
+
+dias = horas / 24
+print("Dias:", dias)
+
+minutos = horas * 60
+print("Minutos:", minutos)
+
+segundos = minutos * 60
+print("Segundos:", segundos)
+
+print("Conversão para escala de tempo: {0:.0f} dia(s), {1:.0f} horas, {2:.2f} minutos e {3:.1f} segundos".format(dias, horas, minutos, segundos))
