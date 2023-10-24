3e5fdc339068ef437c927589fd033b4f67acea92
Bruno Machado <bruno.cadoma@gmail.com>
2023-10-24 11:09:45 -0300

    Transforma celsius em ºF e K

---
+++ temperatura.py
@@ -0,0 +1,7 @@
+celsius = float(input("Digite a temperatura em celsius: "))
+
+fahrenheit = float((celsius * 9/5) + 32)
+kelvin = celsius + 273
+
+print("O valor da temperatura convertida para fahrenheit é:", fahrenheit, "ºF")
+print("O valor da temperatura convertida para Kelvin é:", kelvin, "K")
