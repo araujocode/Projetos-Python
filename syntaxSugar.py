ddce7feba8562a5098ee4b6c8e29611afa7f22ab
Bruno Machado <bruno.cadoma@gmail.com>
2023-10-24 09:21:26 -0300

    Projetos feitos em aula

---
+++ syntaxSugar.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número: "))
+print("O numero informado é:", numero)
+
+numero *= 3
+print("O valor multiplicado por 3 é:", numero),
+
+numero %= 2
+print("O módulo do numero por 2 é:", numero)
