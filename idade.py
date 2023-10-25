4702325bb241f77faf3df76297c15ae1e9afae74
Bruno Machado <bruno.cadoma@gmail.com>
2023-10-25 10:46:46 -0300

    converte idade em meses e dias

--- idade.py
+++ idade.py
@@ -1,6 +1,6 @@
 idade = int(input("Digite sua idade: "))
 meses = idade * 12
-dias = meses * 30
+dias = int (meses * 30.416666666666666666666666666667)
 
 
 print("A idade de {0} anos equivale a {1} meses e {2} dias".format(idade, meses, dias))
