302c394960e15071b5495109cc69f8d5f93c25ab
Bruno Machado <bruno.cadoma@gmail.com>
2023-10-25 11:43:51 -0300

    alterada a formula de dias

--- idade.py
+++ idade.py
@@ -1,6 +1,6 @@
 idade = int(input("Digite sua idade: "))
 meses = idade * 12
-dias = int (meses * 30.416666666666666666666666666667)
+dias = idade * 365
 
 
 print("A idade de {0} anos equivale a {1} meses e {2} dias".format(idade, meses, dias))
