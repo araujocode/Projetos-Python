d8511449c28660110a5a375d8357453703b15361
Bruno Machado <bruno.cadoma@gmail.com>
2023-11-13 10:44:30 -0300

    tabuada com whiles aninhados

---
+++ tabuadaCompleta.py
@@ -0,0 +1,11 @@
+cont1 = 1
+
+while cont1 <= 10:
+    cont2 = 1
+    print("\n")
+    while cont2 <= 10:
+        resultado = cont1 * cont2
+        print(f"{cont1} x {cont2} = {resultado}")
+        cont2 += 1
+
+    cont1 += 1
